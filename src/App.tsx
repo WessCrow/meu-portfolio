10e15a88f62759202a683d87d9d22a223cfbbe5d
martinsbicudo <victor.mbicudo@gmail.com>
2026-04-10 20:47:21 -0300

    keep only wtype

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import GridEngine from './components/GridEngine';
-
-export default function App() {
-  return (
-    <div className="w-full h-full">
-      <GridEngine />
-    </div>
-  );
-}
--- src/types/index.ts
+++
@@ -1,9 +0,0 @@
-// PRD definition
-export interface Cell {
-  x: number;
-  y: number;
-  active: boolean;
-}
-
-// Key string format "x,y"
-export type CellKey = `${number},${number}`;
